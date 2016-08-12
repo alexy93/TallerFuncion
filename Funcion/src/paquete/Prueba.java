e7b407b13b2448d5ecd38cd6d4b237b224eaeee3
sthongurdt <sthongurdt@hotmail.com>
2016-08-12 11:30:33 -0500

    Prueba codigo de Potencia

--- Funcion/src/paquete/Prueba.java
+++ Funcion/src/paquete/Prueba.java
@@ -4,6 +4,8 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Potencia P = new Potencia();
+		System.out.println(P.poten(2, 3));	
 
 	}
 
